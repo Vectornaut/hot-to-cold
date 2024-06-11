198e58648cc019d30b76c26725a0a17b45cd5dcb
Aaron Fenyes <aaron.fenyes@fareycircles.ooo>
2024-06-11 03:57:24 -0700

    Implement vertex dragging
    This reveals that the boundary distance is wrong when the quadrilateral
    is non-convex.

---
+++ Handle.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class Handle : Node2D {
+  [Signal] public delegate void DragEventHandler(Vector2 position);
+  
+  private float _radius;
+  private float _radiusSq;
+  private bool _dragging = false;
+  private Vector2 _dragOffset;
+  
+  public Handle(Vector2 position, float radius = 6f) {
+    Position = position;
+    _radius = radius;
+    _radiusSq = radius * radius;
+  }
+  
+  public override void _Draw() {
+    DrawCircle(Vector2.Zero, _radius, _dragging ? Colors.Red : Colors.Orange);
+  }
+  
+  // drag handling, following the Godot mouse motion tutorial
+  //
+  //   https://docs.godotengine.org/en/stable/tutorials/inputs/input_examples.html#mouse-motion
+  //
+  public override void _Input(InputEvent ev) {
+    if (ev is InputEventMouseButton evButton && evButton.ButtonIndex == MouseButton.Left) {
+      Vector2 locEvPos = ToLocal(evButton.Position);
+      if (!_dragging && evButton.Pressed && locEvPos.LengthSquared() < _radiusSq) {
+        _dragging = true;
+        _dragOffset = locEvPos;
+        QueueRedraw();
+      } else if (_dragging && !evButton.Pressed) {
+        _dragging = false;
+        QueueRedraw();
+      }
+      _dragging &= evButton.Pressed;
+    } else if (ev is InputEventMouseMotion evMotion && _dragging) {
+      this.GlobalPosition = evMotion.Position - _dragOffset;
+      EmitSignal(SignalName.Drag, this.GlobalPosition);
+    }
+  }
+}
--- HeatQuad.cs
+++ HeatQuad.cs
@@ -2,27 +2,41 @@ using Godot;
 using System;
 
 public partial class HeatQuad : Polygon2D {
+  private Vector2[] _Vertices;
+  private Vector3[] _Sides;
+  private ShaderMaterial _ShaderMat;
+  
   public override void _Ready() {
     // move to center of screen
     Vector2 res = GetViewportRect().Size;
     Position = 0.5f * res;
     
-    Polygon = new Vector2[] {
-      new Vector2(-0.2f * res.X, 0.2f * res.Y),
-      new Vector2(0, -0.4f * res.Y),
-      new Vector2(0.2f * res.X, -0.2f * res.Y),
-      new Vector2(0, 0.4f * res.Y)
+    // create vertex handles
+    Handle leftHandle = new Handle(new Vector2(-0.2f * res.X, 0.2f * res.Y));
+    Handle rightHandle = new Handle(new Vector2(0.2f * res.X, -0.2f * res.Y));
+    leftHandle.Drag += OnLeftHandleDrag;
+    rightHandle.Drag += OnRightHandleDrag;
+    AddChild(leftHandle);
+    AddChild(rightHandle);
+    
+    // create vertices
+    _Vertices = new Vector2[] {
+      leftHandle.Position,
+      new Vector2(0, -0.3f * res.Y),
+      rightHandle.Position,
+      new Vector2(0, 0.3f * res.Y)
     };
     
     // find side distance functions
-    Vector3[] sides = new Vector3[4];
-    for (int k = 0; k < sides.Length; k++) {
-      sides[k] = Side(Polygon[k], Polygon[(k+1) % Polygon.Length]);
+    _Sides = new Vector3[4];
+    for (int k = 0; k < _Sides.Length; k++) {
+      _Sides[k] = Side(_Vertices[k], _Vertices[(k+1) % _Vertices.Length]);
     }
     
-    // pass side equations to shader
-    ShaderMaterial shaderMat = this.Material as ShaderMaterial;
-    shaderMat.SetShaderParameter("sides", sides);
+    // save a reference to the shader material
+    _ShaderMat = this.Material as ShaderMaterial;
+    
+    LoadVerticesAndSides();
   }
   
   // in homogeneous coordinates, the inner product with this vector gives the
@@ -33,5 +47,32 @@ public partial class HeatQuad : Polygon2D {
     Vector2 dir = start.DirectionTo(end);
     Vector2 normal = new Vector2(-dir.Y, dir.X);
     return new Vector3(normal.X, normal.Y, -normal.Dot(start));
+  }
+  
+  // set the vertex array and pass the side equations to shader
+  private void LoadVerticesAndSides() {
+    Polygon = _Vertices;
+    _ShaderMat.SetShaderParameter("sides", _Sides);
+  }
+  
+  private void UpdateVertex(int k, Vector2 v) {
+    // update vertex
+    _Vertices[k] = v;
+    
+    // update sides
+    int prev = (k + _Vertices.Length - 1) % _Vertices.Length;
+    int next = (k + 1) % _Vertices.Length;
+    _Sides[prev] = Side(_Vertices[prev], _Vertices[k]);
+    _Sides[k] = Side(_Vertices[k], _Vertices[next]);
+    
+    LoadVerticesAndSides();
+  }
+  
+  private void OnLeftHandleDrag(Vector2 position) {
+    UpdateVertex(0, ToLocal(position));
+  }
+  
+  private void OnRightHandleDrag(Vector2 position) {
+    UpdateVertex(2, ToLocal(position));
   }
 }
