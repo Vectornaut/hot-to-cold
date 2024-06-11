5cefabf4aebabfff9e6b5caf5fda34f8c34b1eee
Aaron Fenyes <aaron.fenyes@fareycircles.ooo>
2024-06-11 14:27:48 -0700

    Render HeatQuad at hi res to use more GPU units

--- HeatQuad.cs
+++ HeatQuad.cs
@@ -11,26 +11,22 @@ public partial class HeatQuad : Polygon2D {
     Vector2 res = GetViewportRect().Size;
     Position = 0.5f * res;
     
-    // create vertex handles
-    Handle leftHandle = new Handle(new Vector2(-0.2f * res.X, 0.2f * res.Y));
-    Handle rightHandle = new Handle(new Vector2(0.2f * res.X, -0.2f * res.Y));
-    leftHandle.Drag += OnLeftHandleDrag;
-    rightHandle.Drag += OnRightHandleDrag;
-    AddChild(leftHandle);
-    AddChild(rightHandle);
-    
     // create vertices
     _Vertices = new Vector2[] {
-      leftHandle.Position,
-      new Vector2(0, -0.3f * res.Y),
-      rightHandle.Position,
-      new Vector2(0, 0.3f * res.Y)
+      new Vector2(-0.3f * res.X, 0.1f * res.Y),
+      new Vector2(0, -0.2f * res.Y),
+      new Vector2(0.3f * res.X, -0.1f * res.Y),
+      new Vector2(0, 0.2f * res.Y)
     };
     Polygon = _Vertices;
     
     // load vertices into shader
     _ShaderMat = this.Material as ShaderMaterial;
     _ShaderMat.SetShaderParameter("vertices", _Vertices);
+  }
+  
+  public Vector2 GetVertex(int k) {
+    return _Vertices[k];
   }
   
   // in homogeneous coordinates, the inner product with this vector gives the
@@ -49,11 +45,11 @@ public partial class HeatQuad : Polygon2D {
     _ShaderMat.SetShaderParameter("vertices", _Vertices);
   }
   
-  private void OnLeftHandleDrag(Vector2 position) {
+  public void OnLeftHandleDrag(Vector2 position) {
     UpdateVertex(0, ToLocal(position));
   }
   
-  private void OnRightHandleDrag(Vector2 position) {
+  public void OnRightHandleDrag(Vector2 position) {
     UpdateVertex(2, ToLocal(position));
   }
 }
---
+++ HeatQuadDemo.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class HeatQuadDemo : Node2D {
+  private TextureRect _Display;
+  private SubViewport _View;
+  private HeatQuad _Quad;
+  private Vector2 _Oversampling;
+  
+  public override void _Ready() {
+    // get display and viewport
+    _Display = GetNode("Display") as TextureRect;
+    _View = GetNode("QuadViewport") as SubViewport;
+    _Oversampling = _View.Size / _Display.Size;
+    
+    // get quadrilateral
+    this._Quad = _View.GetNode("HeatQuad") as HeatQuad;
+    
+    // create vertex handles
+    Handle leftHandle = new Handle(ViewToDisplay(_Quad.ToGlobal(_Quad.GetVertex(0))));
+    Handle rightHandle = new Handle(ViewToDisplay(_Quad.ToGlobal(_Quad.GetVertex(2))));
+    leftHandle.Drag += OnLeftHandleDrag;
+    rightHandle.Drag += OnRightHandleDrag;
+    AddChild(leftHandle);
+    AddChild(rightHandle);
+  }
+  
+  private Vector2 DisplayToView(Vector2 displayPoint) {
+    Vector2 viewPoint = displayPoint;
+    if (_Display.FlipV) {
+      viewPoint.Y = _Display.Size.Y -  viewPoint.Y;
+    }
+    viewPoint *= _Oversampling;
+    return viewPoint;
+  }
+  
+  private Vector2 ViewToDisplay(Vector2 viewPoint) {
+    Vector2 displayPoint = viewPoint / _Oversampling;
+    if (_Display.FlipV) {
+      displayPoint.Y = _Display.Size.Y -  displayPoint.Y;
+    }
+    return displayPoint;
+  }
+  
+  private void OnLeftHandleDrag(Vector2 displayPosition) {
+    _Quad.OnLeftHandleDrag(DisplayToView(displayPosition));
+    _View.RenderTargetUpdateMode = SubViewport.UpdateMode.Once;
+  }
+  
+  private void OnRightHandleDrag(Vector2 displayPosition) {
+    _Quad.OnRightHandleDrag(DisplayToView(displayPosition));
+    _View.RenderTargetUpdateMode = SubViewport.UpdateMode.Once;
+  }
+}
