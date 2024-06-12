a8b0502b03b81c8c0916c41e1ff0feec6f19ebee
Aaron Fenyes <aaron.fenyes@fareycircles.ooo>
2024-06-11 21:04:12 -0700

    Fix up formatting
    Also, make the viewport exactly twice the size of the display.

--- Handle.cs
+++ Handle.cs
@@ -5,27 +5,27 @@ public partial class Handle : Node2D {
   [Signal] public delegate void DragEventHandler(Vector2 position);
   [Signal] public delegate void GrabEventHandler(bool grabbed);
   
-  private float _radius;
-  private float _radiusSq;
-  private bool _grabbed = false;
-  private Vector2 _dragOffset;
+  private float _Radius;
+  private float _RadiusSq;
+  private bool _Grabbed = false;
+  private Vector2 _DragOffset;
   
   public Handle(Vector2 position, float radius = 6f) {
     Position = position;
-    _radius = radius;
-    _radiusSq = radius * radius;
+    _Radius = radius;
+    _RadiusSq = radius * radius;
     
     // always process
     ProcessMode = ProcessModeEnum.Always;
   }
   
   private void SetGrabbed(bool grabbed) {
-    _grabbed = grabbed;
+    _Grabbed = grabbed;
     EmitSignal(SignalName.Grab, grabbed);
   }
   
   public override void _Draw() {
-    DrawCircle(Vector2.Zero, _radius, _grabbed ? Colors.Red : Colors.Orange);
+    DrawCircle(Vector2.Zero, _Radius, _Grabbed ? Colors.Red : Colors.Orange);
   }
   
   // drag handling, following the Godot mouse motion tutorial
@@ -35,16 +35,16 @@ public partial class Handle : Node2D {
   public override void _Input(InputEvent ev) {
     if (ev is InputEventMouseButton evButton && evButton.ButtonIndex == MouseButton.Left) {
       Vector2 locEvPos = ToLocal(evButton.Position);
-      if (!_grabbed && evButton.Pressed && locEvPos.LengthSquared() < _radiusSq) {
+      if (!_Grabbed && evButton.Pressed && locEvPos.LengthSquared() < _RadiusSq) {
+        _DragOffset = locEvPos;
         SetGrabbed(true);
-        _dragOffset = locEvPos;
         QueueRedraw();
-      } else if (_grabbed && !evButton.Pressed) {
+      } else if (_Grabbed && !evButton.Pressed) {
         SetGrabbed(false);
         QueueRedraw();
       }
-    } else if (ev is InputEventMouseMotion evMotion && _grabbed) {
-      GlobalPosition = evMotion.Position - _dragOffset;
+    } else if (ev is InputEventMouseMotion evMotion && _Grabbed) {
+      GlobalPosition = evMotion.Position - _DragOffset;
       EmitSignal(SignalName.Drag, GlobalPosition);
     }
   }
